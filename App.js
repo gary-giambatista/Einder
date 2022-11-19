004975cb48a0686c1261dd05abbcab89551fff0f
Gary Giambatista <gary.giambatista@gmail.com>
2022-11-19 15:11:24 -0500

    removed broken tailwin-rn package.

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { useTailwind } from "tailwind-rn";
+
 import { AuthContextProvider } from "./hooks/useAuth";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
-	const tailwind = useTailwind();
-
 	return (
 		<NavigationContainer>
 			<AuthContextProvider>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,24 @@
-import React from "react";
-import { Button, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { TwitterAuthProvider } from "firebase/auth";
+import React, { useLayoutEffect } from "react";
+import {
+	Button,
+	ImageBackground,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
 import { useAuth } from "../hooks/useAuth";
 
 const LoginScreen = () => {
 	const { googleSignIn, loading } = useAuth();
+	const navigation = useNavigation();
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerShown: false,
+		});
+	}, []);
 
 	const handleGoogleSignIn = async () => {
 		try {
@@ -15,8 +30,15 @@ const LoginScreen = () => {
 
 	return (
 		<View>
-			<Text>{loading ? "Loading..." : "Login to the app."}</Text>
-			<Button title="Login" onPress={handleGoogleSignIn} />
+			<ImageBackground
+				resizeMode="cover"
+				source={{ uri: "https://tinder.com/static/tinder.png" }}
+			>
+				<Button title="Login" onPress={handleGoogleSignIn} />
+				<TouchableOpacity>
+					<Text>Sign in & get swipping</Text>
+				</TouchableOpacity>
+			</ImageBackground>
 		</View>
 	);
 };
