e7bdeb9f617c207d020a3d332b37ef4ec6076513
Gary Giambatista <gary.giambatista@gmail.com>
2022-11-20 00:11:42 -0500

    added react-native-deck-swipper and finished most of the home page. Implemented cards and buttons for swiping.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,57 @@
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useRef } from "react";
 import {
 	Button,
 	Image,
 	SafeAreaView,
+	StyleSheet,
 	Text,
 	TouchableOpacity,
 	View,
 } from "react-native";
+import Swiper from "react-native-deck-swiper";
 import { useTailwind } from "tailwind-rn";
 import { useAuth } from "../hooks/useAuth";
+
+const DUMMY_DATA = [
+	{
+		id: 0,
+		firstName: "Brad",
+		lastName: "Pitt",
+		job: "Actor",
+		photoURL:
+			"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY3MDUxMjkzMjI1OTIwMTcz/brad-pitt-attends-the-premiere-of-20th-century-foxs--square.jpg",
+		age: 58,
+	},
+	{
+		id: 1,
+		firstName: "Megan",
+		lastName: "Fox",
+		job: "Actor",
+		photoURL:
+			"https://media.glamour.com/photos/626ac4239bffe69d175b7e0a/master/w_2560%2Cc_limit/1324443084",
+		age: 36,
+	},
+	{
+		id: 2,
+		firstName: "Oliva",
+		lastName: "Wilde",
+		job: "Actor",
+		photoURL:
+			"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1662647826.jpg?crop=1.00xw:0.669xh;0,0.128xh&resize=640:*",
+		age: 38,
+	},
+];
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
 	const { user, logOut } = useAuth();
 	const tailwind = useTailwind();
-	console.log(user);
+	const swipeRef = useRef(null);
+
 	return (
-		<SafeAreaView>
+		<SafeAreaView style={tailwind("flex-1")}>
 			{/* Header */}
 			<View
 				style={tailwind(
@@ -33,7 +66,7 @@ const HomeScreen = () => {
 				</TouchableOpacity>
 				<TouchableOpacity style={tailwind("")}>
 					<Image
-						style={tailwind("h-[56px] w-[47px] object-contain")}
+						style={tailwind("h-[56px] w-[47px]")}
 						source={require("../TinderLogo.png")}
 					/>
 				</TouchableOpacity>
@@ -42,8 +75,94 @@ const HomeScreen = () => {
 					<Ionicons name="chatbubbles-sharp" size={30} color="#FF5864" />
 				</TouchableOpacity>
 			</View>
-
 			{/* end of header */}
+
+			<View style={tailwind("flex-1 -mt-6")}>
+				<Swiper
+					ref={swipeRef}
+					containerStyle={{ backgroundColor: "transparent" }}
+					cards={DUMMY_DATA}
+					stackSize={5}
+					cardIndex={0}
+					backgroundColor={"4FD0E9"}
+					animateCardOpacity
+					verticalSwipe={false}
+					onSwipedLeft={() => {
+						console.log("Swipe Pass");
+					}}
+					onSwipedRight={() => {
+						console.log("Swipe Match");
+					}}
+					overlayLabels={{
+						left: {
+							title: "NOPE",
+							style: {
+								label: {
+									textAlign: "right",
+									color: "red",
+								},
+							},
+						},
+						right: {
+							title: "MATCH",
+							style: {
+								label: {
+									color: "#4DED30",
+								},
+							},
+						},
+					}}
+					renderCard={(card) => (
+						<View
+							id={card.id}
+							style={tailwind("relative h-3/4 bg-white rounded-xl")}
+						>
+							<Image
+								style={tailwind("absolute top-0 h-full w-full rounded-xl")}
+								source={card.photoURL}
+							/>
+							<View
+								style={[
+									tailwind(
+										"absolute bottom-0 h-full bg-white w-full flex-row justify-between items-center h-20 px-6 py-2 rounded-b-xl"
+									),
+									styles.cardShadow,
+								]}
+							>
+								<View>
+									<Text style={tailwind("text-xl font-bold")}>
+										{card.firstName} {card.lastName}
+									</Text>
+									<Text> {card.job} </Text>
+								</View>
+								<Text style={tailwind("text-2xl font-bold ")}>
+									{" "}
+									{card.age}{" "}
+								</Text>
+							</View>
+						</View>
+					)}
+				/>
+			</View>
+			<View style={tailwind("flex flex-row justify-evenly")}>
+				<TouchableOpacity
+					onPress={() => swipeRef.current.swipeLeft()}
+					style={tailwind(
+						"items-center justify-center rounded-full w-16 h-16 bg-red-200 mb-4"
+					)}
+				>
+					<Entypo name="cross" size={24} color="red" />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => swipeRef.current.swipeRight()}
+					style={tailwind(
+						"items-center justify-center rounded-full w-16 h-16 bg-green-200 mb-4"
+					)}
+				>
+					<AntDesign name="heart" size={24} color="green" />
+				</TouchableOpacity>
+			</View>
+
 			{/* <Text>I am the home screen.</Text>
 			<Button
 				title="Go to Chat screen."
@@ -55,3 +174,17 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+	cardShadow: {
+		shadowColor: "000",
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+
+		elevation: 2,
+	},
+});
